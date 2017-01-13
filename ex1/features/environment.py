f4f4adfe30692c6e0e4c3f7415220d2dae13b5d1
kowalcj0 <janusz@justyoyo.com>
2017-01-13 16:29:08 +0000

    added behave environment.py that deals with scenario hooks and logging

---
+++ ex1/features/environment.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""Behave configuration file."""
+
+import os
+import logging
+
+import requests
+import pprint
+
+
+def before_scenario(context, scenario):
+    logging.debug('Scenario: {}'.format(scenario.name))
+    # clear the stored response object before every scenario
+    context.response = None
+
+
+def before_step(context, step):
+    logging.debug('Step: {} {}'.format(step.step_type, str(repr(step.name))))
+
+
+def before_all(context):
+    # API URL under test
+    context.platform_url = 'https://api.stackexchange.com'
+    context.test_url = '{}{}'.format(
+        context.platform_url,
+        '/2.2/search?order=desc&sort=activity&site=stackoverflow&intitle=')
+
+    # requests configuration
+    context.request_timeout = 20
+
+    # get the root logger
+    rootLogger = logging.getLogger()
+
+    # configure the formatter
+    fmt = ('%(asctime)s-%(filename)s[line:%(lineno)d]-%(name)s-%(levelname)s: '
+           '%(message)s')
+    logFormatter = logging.Formatter(fmt)
+
+    # configure the file logger
+    logFile = "./reports/behave.log"
+    fileHandler = logging.FileHandler(logFile)
+    fileHandler.setLevel(logging.DEBUG)
+    fileHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(fileHandler)
+
+    # configure the console logger
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    consoleHandler.setLevel(logging.ERROR)
+    rootLogger.addHandler(consoleHandler)
+
+    # Check if the Stack Exchange API is reachable
+    try:
+        requests.get(context.platform_url, timeout=context.request_timeout)
+    except requests.ConnectionError:
+        raise Exception(
+            ('Unable to connect to the test URL {} - please ensure that '
+             'Stack Exchange API is reachable'.format(context.platform_url)))
+
+
+def after_step(context, step):
+    if step.status == "failed":
+        msg = 'Step "{} {}" has failed. Reason: "{}".'.format(
+            step.step_type, step.name, step.exception)
+        logging.debug(msg)
