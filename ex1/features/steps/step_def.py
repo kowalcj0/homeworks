36de250a526ec89d390723507ad46e8694604c4b
kowalcj0 <janusz@justyoyo.com>
2017-01-13 18:52:34 +0000

    added step definitions for comparing 2 JSONs

--- ex1/features/steps/step_def.py
+++ ex1/features/steps/step_def.py
@@ -5,6 +5,8 @@ from behave import given
 from behave import when
 from behave import then
 
+from step_impl import compare_original_response_with_copy
+from step_impl import copy_response
 from step_impl import every_existing_owner_should_have_valid_profile_image_link
 from step_impl import every_existing_owner_should_have_valid_profile_link
 from step_impl import every_non_existing_owner_should_not_have_profile_image_link
@@ -13,12 +15,13 @@ from step_impl import get_activity_feed
 from step_impl import items_should_contain_array_of_tags
 from step_impl import items_should_contain_owner_object
 from step_impl import items_should_contain_required_values
+from step_impl import remove_field_from_every_item_in_response_copy
+from step_impl import replace_field_value_in_every_item_in_response_copy
 from step_impl import response_contains_array_named
 from step_impl import response_contains_meta_data
 from step_impl import save_tags
 from step_impl import stack_exchange_is_reachable
 from step_impl import tags_list_shoul_have_no_duplicates
-
 
 @given('that Stack Exchange API is reachable')
 def step_stack_exchange_is_reachable(context):
@@ -83,3 +86,22 @@ def step_save_tags(context):
 @then('we should have a list of tags without duplicates')
 def step_tags_list_shoul_have_no_duplicates(context):
     tags_list_shoul_have_no_duplicates(context)
+
+
+@when('we create a copy of the response')
+def step_copy_response(context):
+    copy_response(context)
+
+
+@when('we replace the value of field "{name}" in every item of the response copy with "{value}"')
+def step_replace_field_value_in_every_item_in_response_copy(context, name, value):
+    replace_field_value_in_every_item_in_response_copy(context, name, value)
+
+
+@when('we remove the field "{name}" from every item of the response copy')
+def step_remove_field_from_every_item_in_response_copy(context, name):
+    remove_field_from_every_item_in_response_copy(context, name)
+
+@then('the original response and the copy should be identical except for title and last_activity_date values')
+def step_compare_original_response_with_copy(context):
+    compare_original_response_with_copy(context)
